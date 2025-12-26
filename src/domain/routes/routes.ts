30fab1c776601655d4e9192f2d43e4a30bf8e655
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-12-26 20:11:25 +0100

    feat(notfound): Add NotFound page and update routing to handle 404 errors

--- src/domain/routes/routes.ts
+++ src/domain/routes/routes.ts
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router";
 import App from "@/App";
+import NotFound from "@/presentation/pages/NotFound";
 
 const routes = createBrowserRouter([
     {
         path: "/",
         Component: App,
+    },
+    {
+        path: "*",
+        Component: NotFound,
     },
 ]);
 
