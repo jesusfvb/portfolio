ea3c1ac4c34fcf01998334bb8f7d208451f9941f
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-01-05 18:07:32 +0100

    feat(ErrorBoundary): add ErrorPage component and integrate into routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import routes from "@/domain/routes/routes";
 import "@/presentation/styles/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 
 function App() {
--- src/domain/routes/routes.ts
+++ src/domain/routes/routes.ts
@@ -1,20 +1,25 @@
-import { createBrowserRouter } from "react-router";
+import { ErrorPage } from "@/presentation/pages/error-boundary";
 import HomePage from "@/presentation/pages/home/HomePage";
 import NotFoundPage from "@/presentation/pages/not-found/NotFoundPage";
 import ProjectDetailPage from "@/presentation/pages/project-detail/ProjectDetailPage";
+import { createBrowserRouter } from "react-router";
+
 
 const routes = createBrowserRouter([
     {
         path: "/",
         Component: HomePage,
+        ErrorBoundary: ErrorPage,
     },
     {
         path: "/projects/:id",
         Component: ProjectDetailPage,
+        ErrorBoundary: ErrorPage,
     },
     {
         path: "*",
         Component: NotFoundPage,
+        ErrorBoundary: ErrorPage,
     },
 ]);
 
