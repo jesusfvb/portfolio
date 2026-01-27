70541a052da7ed3c3c8fdf704afc128847b5e2f6
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-01-27 19:34:46 +0100

    refactor(Projects): remove ProjectOverlay component and update exports

--- src/presentation/features/home/components/ProjectsSection/components/card/index.ts
+++ src/presentation/features/home/components/ProjectsSection/components/card/index.ts
@@ -1,5 +1,4 @@
 export { default as ProjectCard } from './ProjectCard';
 export { default as ProjectCardContent } from './ProjectCardContent';
 export { default as ProjectBanner } from './ProjectBanner';
-export { default as ProjectOverlay } from './ProjectOverlay';
 export { default as ProjectTechStack } from './ProjectTechStack';
