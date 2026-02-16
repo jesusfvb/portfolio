6cf16175439ec4070ae68ec24b747a26a7243197
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-16 19:12:28 +0100

    feat(i18n): add internationalization setup with i18next and react-i18next

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "@/presentation/styles/index.css";
+import "@/application/i18n";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
